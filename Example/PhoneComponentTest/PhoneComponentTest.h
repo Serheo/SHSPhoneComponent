9d6c287389be3d4e9789ded40570f337bb6d82a0
Serheo Shatunov <sshatunov@yandex.ru>
2013-05-04 17:09:01 +0400

    tests ci

---
+++ Example/PhoneComponentTest/PhoneComponentTest.h
@@ -0,0 +1,13 @@
+//
+//  PhoneComponentTest.h
+//  PhoneComponentTest
+//
+//  Created by Willy on 04.05.13.
+//  Copyright (c) 2013 SHS. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PhoneComponentTest : SenTestCase
+
+@end
---
+++ Example/PhoneComponentTest/PhoneComponentTest.m
@@ -0,0 +1,34 @@
+//
+//  PhoneComponentTest.m
+//  PhoneComponentTest
+//
+//  Created by Willy on 04.05.13.
+//  Copyright (c) 2013 SHS. All rights reserved.
+//
+
+#import "PhoneComponentTest.h"
+
+@implementation PhoneComponentTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    NSLog(@"TESTTESTEST");
+    STAssertEquals(1, 1, @"test");
+  //  STFail(@"Unit tests are not implemented yet in PhoneComponentTest");
+}
+
+@end
